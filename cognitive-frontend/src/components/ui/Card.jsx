4b0692e833d58218b09e8b0f1d51bc5fbc53ab86
rozhina rafiee <154349195+rozhrafiee@users.noreply.github.com>
2026-02-10 15:53:05 +0330

    Delete cognitive-frontend directory

--- cognitive-frontend/src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '../../lib/utils';
-
-export default function Card({ className, ...props }) {
-  return (
-    <div className={cn('surface p-5', className)} {...props} />
-  );
-}
--- cognitive-frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/* Vite env types - manual declaration to avoid loading vite/client type definition */
-interface ImportMetaEnv {
-  readonly VITE_API_BASE?: string;
-  [key: string]: string | boolean | undefined;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
